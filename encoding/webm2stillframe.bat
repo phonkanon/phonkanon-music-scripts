844143636ce8726791594307ee17fdb733d9a0a2
phonkanon <144072743+phonkanon@users.noreply.github.com>
2023-09-09 10:53:34 -0500

    Update webm2stillframe.bat

--- encoding/webm2stillframe.bat
+++ encoding/webm2stillframe.bat
@@ -128,7 +128,7 @@ if /I "%FORMAT%" == "WEBM" (
     echo Video Dimensions: %coverwidth%x%coverwidth%
     echo Approx. Video Size: %approxsize%
     echo --------------------------------------
-    ffmpeg -loop 1 -framerate 1 -loglevel error -stats -i "%coverfilename%" -i "%audiofilename%" -c:a libopus -c:v libvpx-vp9 -crf 30 -cpu-used 5 -tile-columns 3 -threads 0 -shortest -strict -2 -vbr on -compression_level 10 "!filename!.webm"
+    ffmpeg -loop 1 -framerate 1 -loglevel error -stats -i "%coverfilename%" -i "%audiofilename%" -c:a libopus -c:v libvpx-vp9 -crf 30 -cpu-used 5 -tile-columns 3 -threads 0 -t %duration% -strict -2 -vbr on -compression_level 10 "!filename!.webm"
     del %audiofilename%
     del %coverfilename%
     exit /b
